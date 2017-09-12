96a637befd7a59cfaf754a3b185f8d01943a354d
Divesh Mavji <divesh987@hotmail.co.uk>
2017-09-12 10:20:53 +0100

    complete with 0 failures

--- environment/provision.sh
+++ environment/provision.sh
@@ -2,17 +2,15 @@
 sudo apt-get update -y
 sudo apt-get upgrade -y
 sudo apt-get install nginx -y
-# sudo gpg2 --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
-# sudo gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-# curl -sSL https://rvm.io/mpapis.asc | sudo gpg --import -
-# source /etc/profile.d/rvm.sh
-# \curl -sSL https://get.rvm.io | bash -s stable
-# rvm install 2.4.0 
 
-# sudo apt-get install Ruby ruby2.4 ruby2.4
-sudo apt-get install ruby -y
-# ruby-install --no-install-deps ruby 2.4.0
-cd app
-gem install bundle
-bundle install
-gem install rails:0.14.4 rubyzip:'< 1'
+#adding a repo to the package source list 
+sudo apt-add-repository ppa:brightbox/ruby-ng
+sudo apt-get update -y
+
+#installs the ruby packages
+sudo apt-get install ruby2.4 ruby2.4-dev ruby-build -y
+#build essential packages,,allows you to compile
+sudo apt-get install build-essential libgmp-dev libxml2 -y
+
+sudo gem install bundler
+sudo gem install rails
